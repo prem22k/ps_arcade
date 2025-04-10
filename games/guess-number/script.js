80e94f020ab75e7aa9cbce974cb3be1b1cc7dc26
prem22k <premsai224k@gmail.com>
2025-04-10 19:45:00 +0530

    script: extract guess-number js logic

---
+++ games/guess-number/script.js
@@ -0,0 +1,24 @@
+var globalTarget = Math.floor(Math.random() * 100) + 1;
+function resetGuessTarget() {
+    globalTarget = Math.floor(Math.random() * 100) + 1;
+    alert("new target generated!");
+}
+
+function startGuessGame() {
+    document.getElementById('guess-game').style.backgroundColor = 'yellow';
+    var target = globalTarget;
+    var guess = -1;
+    var attempts = 0;
+    while (guess != target) {
+        var userGuess = window.prompt("Guess 1-100:");
+        guess = parseInt(userGuess);
+        attempts++;
+        document.getElementById('last-guess').innerText = "Last Guess: " + guess;
+        if (guess > target) {
+            alert("Too high!");
+        } else if (guess < target) {
+            alert("Too low!");
+        }
+    }
+    alert("you win! it took you " + attempts + " attempts.");
+}
