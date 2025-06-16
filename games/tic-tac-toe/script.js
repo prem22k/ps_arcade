389525e3001c2b311a1ecbeb70c01d6f576c2c59
prem22k <premsai224k@gmail.com>
2025-06-16 11:30:00 +0530

    script: add event listeners to grid cells

--- games/tic-tac-toe/script.js
+++ games/tic-tac-toe/script.js
@@ -2,3 +2,10 @@
 var board = ["", "", "", "", "", "", "", "", ""];
 var currentPlayer = "X";
 var gameActive = true;
+
+var cells = document.querySelectorAll('.cell');
+cells.forEach(function(cell) {
+    cell.addEventListener('click', function() {
+        console.log('cell clicked event');
+    });
+});
