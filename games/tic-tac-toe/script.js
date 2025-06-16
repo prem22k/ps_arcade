26b0168cf0ecba93457a093f18206de6b41b52b0
prem22k <premsai224k@gmail.com>
2025-06-16 15:30:00 +0530

    script: add simple player turn indicator text

--- games/tic-tac-toe/script.js
+++ games/tic-tac-toe/script.js
@@ -3,17 +3,19 @@ var currentPlayer = "X";
 var gameActive = true;
 
 var cells = document.querySelectorAll('.cell');
+var turnIndicator = document.getElementById('player-turn');
+
 cells.forEach(function(cell) {
     cell.addEventListener('click', function() {
         if (!gameActive) return;
         var index = parseInt(cell.getAttribute('data-index'));
         
-        // validation check to prevent overwriting
         if (board[index] !== "") return;
         
         board[index] = currentPlayer;
         cell.innerText = currentPlayer;
         
         currentPlayer = (currentPlayer === "X") ? "O" : "X";
+        turnIndicator.innerText = "Player " + currentPlayer + "'s Turn";
     });
 });
