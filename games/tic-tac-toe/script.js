c8e48792cd16dc72d4a75d428281c399b9e347dc
prem22k <premsai224k@gmail.com>
2025-06-23 09:30:00 +0530

    script: implement checkWin function loop

--- games/tic-tac-toe/script.js
+++ games/tic-tac-toe/script.js
@@ -5,12 +5,22 @@ var gameActive = true;
 var cells = document.querySelectorAll('.cell');
 var turnIndicator = document.getElementById('player-turn');
 
-// define win conditions
 var winConditions = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8],
     [0, 3, 6], [1, 4, 7], [2, 5, 8],
     [0, 4, 8], [2, 4, 6]
 ];
+
+// loop check for win conditions
+function checkWin() {
+    for (var i = 0; i < winConditions.length; i++) {
+        var cond = winConditions[i];
+        if (board[cond[0]] !== "" && board[cond[0]] === board[cond[1]] && board[cond[0]] === board[cond[2]]) {
+            return true;
+        }
+    }
+    return false;
+}
 
 cells.forEach(function(cell) {
     cell.addEventListener('click', function() {
