efe1b27e70106982d128b8209b5fca6190490e11
prem22k <premsai224k@gmail.com>
2025-06-25 10:00:00 +0530

    refactor: clean up comments and unused variables in ttt script

--- games/tic-tac-toe/script.js
+++ games/tic-tac-toe/script.js
@@ -5,6 +5,7 @@ var gameActive = true;
 var cells = document.querySelectorAll('.cell');
 var turnIndicator = document.getElementById('player-turn');
 
+// win configurations
 var winConditions = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8],
     [0, 3, 6], [1, 4, 7], [2, 5, 8],
