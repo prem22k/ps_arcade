5634ef1bf31308d2127dc77959c35df6f996a29f
prem22k <premsai224k@gmail.com>
2025-06-26 09:00:00 +0530

    script: create random mole picker function

--- games/whack-a-mole/script.js
+++ games/whack-a-mole/script.js
@@ -1,3 +1,19 @@
-// mole controller
 var holes = document.querySelectorAll('.hole');
-console.log('holes loaded:', holes.length);
+var lastHoleIndex = -1;
+
+function randomHole() {
+    var index = Math.floor(Math.random() * holes.length);
+    while (index === lastHoleIndex) {
+        index = Math.floor(Math.random() * holes.length);
+    }
+    lastHoleIndex = index;
+    return holes[index];
+}
+
+function showMole() {
+    holes.forEach(function(hole) {
+        hole.classList.remove('active');
+    });
+    var activeHole = randomHole();
+    activeHole.classList.add('active');
+}
