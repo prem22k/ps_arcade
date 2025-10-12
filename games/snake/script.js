2f2069a8e2362e79dc5003777437fe8fb54969db
prem22k <premsai224k@gmail.com>
2025-10-12 14:30:00 +0530

    feat: bind keypress controls to update snake speed

--- games/snake/script.js
+++ games/snake/script.js
@@ -8,10 +8,24 @@ var snake = [
     {x: 120, y: 200}
 ];
 
+var dx = gridSize;
+var dy = 0;
+
+document.addEventListener('keydown', function(e) {
+    if (e.key === 'ArrowUp') { dx = 0; dy = -gridSize; }
+    else if (e.key === 'ArrowDown') { dx = 0; dy = gridSize; }
+    else if (e.key === 'ArrowLeft') { dx = -gridSize; dy = 0; }
+    else if (e.key === 'ArrowRight') { dx = gridSize; dy = 0; }
+});
+
 function gameLoop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     
-    // Draw snake
+    // Move snake head
+    var head = {x: snake[0].x + dx, y: snake[0].y + dy};
+    snake.unshift(head);
+    snake.pop();
+
     ctx.fillStyle = 'lightgreen';
     snake.forEach(function(part) {
         ctx.fillRect(part.x, part.y, gridSize, gridSize);
