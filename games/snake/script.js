9001e12e04c09911c704b8b47c4fbbe201adb13e
prem22k <premsai224k@gmail.com>
2025-10-12 13:45:00 +0530

    feat: draw snake segments inside active canvas grid

--- games/snake/script.js
+++ games/snake/script.js
@@ -2,9 +2,19 @@ var canvas = document.getElementById('snake-canvas');
 var ctx = canvas.getContext('2d');
 var gridSize = 20;
 
-// Initialize snake
 var snake = [
     {x: 160, y: 200},
     {x: 140, y: 200},
     {x: 120, y: 200}
 ];
+
+function gameLoop() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    // Draw snake
+    ctx.fillStyle = 'lightgreen';
+    snake.forEach(function(part) {
+        ctx.fillRect(part.x, part.y, gridSize, gridSize);
+    });
+}
+setInterval(gameLoop, 150);
