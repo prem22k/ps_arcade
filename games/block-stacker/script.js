131e8bc31bec105919b8f8e96dd024f95a97094d
prem22k <premsai224k@gmail.com>
2026-01-15 12:15:00 +0530

    refactor: clean up start game variables reset logic

--- games/block-stacker/script.js
+++ games/block-stacker/script.js
@@ -2,3 +2,10 @@ var gameActive = false;
 var score = 0;
 var highScore = 0;
 var level = 1;
+
+function resetGame() {
+    score = 0;
+    level = 1;
+    gameActive = true;
+    document.getElementById('score').innerText = score;
+}
