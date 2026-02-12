979d24a5629bc6d4f9165a077a2b2472e84f39dd
prem22k <premsai224k@gmail.com>
2026-02-12 12:00:00 +0530

    style: make colors for blocks cycle randomly

--- games/block-stacker/script.js
+++ games/block-stacker/script.js
@@ -3,6 +3,7 @@ var score = 0;
 var highScore = 0;
 var level = 1;
 var activeBlockWidth = 200;
+var colors = ['#e74c3c', '#3498db', '#f1c40f', '#2ecc71', '#9b59b6', '#e67e22'];
 
 function resetGame() {
     score = 0;
@@ -21,4 +22,6 @@ function updateSwingMax() {
     var craneBlock = document.getElementById('crane-block');
     craneBlock.style.setProperty('--swing-max', (320 - activeBlockWidth) + 'px');
     craneBlock.style.width = activeBlockWidth + 'px';
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    craneBlock.style.backgroundColor = color;
 }
