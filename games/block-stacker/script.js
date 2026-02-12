a8355ab95562203d6e09b97dd59f48e7f126e38a
prem22k <premsai224k@gmail.com>
2026-02-12 11:45:00 +0530

    feat: track swing direction states to avoid jumping positions

--- games/block-stacker/script.js
+++ games/block-stacker/script.js
@@ -2,6 +2,7 @@ var gameActive = false;
 var score = 0;
 var highScore = 0;
 var level = 1;
+var activeBlockWidth = 200;
 
 function resetGame() {
     score = 0;
@@ -14,4 +15,10 @@ function getCurrentLeft() {
     var craneBlock = document.getElementById('crane-block');
     var computed = window.getComputedStyle(craneBlock);
     return parseFloat(computed.left) || 0;
+}
+
+function updateSwingMax() {
+    var craneBlock = document.getElementById('crane-block');
+    craneBlock.style.setProperty('--swing-max', (320 - activeBlockWidth) + 'px');
+    craneBlock.style.width = activeBlockWidth + 'px';
 }
