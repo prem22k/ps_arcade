9db03488739a5a41fe54fe338552e3ed04e20751
prem22k <premsai224k@gmail.com>
2026-05-18 13:45:00 +0530

    feat: implement main entry point js/main.js using ES6 import syntax

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-// main entrypoint shell
+import { scoreTracker } from './scoreTracker.js';
+import { cyberUi } from './cyberUi.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('[SYS_INIT] Launching cyber-arcade system sequence...');
+});
