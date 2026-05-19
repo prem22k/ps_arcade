c2409a1a997957ed8c230a8266e1d60d929455e0
prem22k <premsai224k@gmail.com>
2026-05-19 15:30:00 +0530

    feat: implement advanced text glitch rendering animations on card headers

--- js/cyberUi.js
+++ js/cyberUi.js
@@ -3,3 +3,16 @@ export const cyberUi = {
         console.log('[SYS_UI] Sound registry toggling system active...');
     }
 };
+export const glitchText = (el) => {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789//_@#$';
+    const original = el.innerText;
+    let iterations = 0;
+    const interval = setInterval(() => {
+        el.innerText = original.split('').map((c, i) => {
+            if (i < iterations) return original[i];
+            return chars[Math.floor(Math.random() * chars.length)];
+        }).join('');
+        if (iterations >= original.length) clearInterval(interval);
+        iterations += 1/3;
+    }, 30);
+};
