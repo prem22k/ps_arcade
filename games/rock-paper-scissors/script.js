a0b204af22af912ad9b5123ea6e8fdc89de1043b
prem22k <premsai224k@gmail.com>
2026-05-23 11:15:00 +0530

    feat: architect cyber-rps engine constructor and state maps

--- games/rock-paper-scissors/script.js
+++ games/rock-paper-scissors/script.js
@@ -1,44 +1,10 @@
-var userScore = 0;
-var compScore = 0;
-var userName = window.prompt("Enter your name:", "Player");
-if (!userName) userName = "Player";
-document.getElementById('welcome').innerText = "welcome " + userName + "!";
-updateScoreText();
-
-function play(choice) {
-    var choices = ['rock', 'paper', 'scissors'];
-    var compChoice = choices[Math.floor(Math.random() * 3)];
-    
-    var result = "";
-    if (choice === compChoice) {
-        result = "tie";
-        document.getElementById('score').style.color = "orange";
-    } else if (
-        (choice === 'rock' && compChoice === 'scissors') ||
-        (choice === 'paper' && compChoice === 'rock') ||
-        (choice === 'scissors' && compChoice === 'paper')
-    ) {
-        result = "win";
-        userScore++;
-        document.getElementById('score').style.color = "green";
-    } else {
-        result = "lose";
-        compScore++;
-        document.getElementById('score').style.color = "red";
+// Step 8: feat: architect cyber-rps engine constructor and state maps
+// Cyberpunk RPS Engine skeleton
+class CyberRPS {
+    constructor() {
+        this.userScore = 0;
+        this.compScore = 0;
+        this.consecutiveWins = 0;
+        this.highScore = 0;
     }
-    
-    updateScoreText();
-    alert('you chose ' + choice + ', computer chose ' + compChoice + '.\nresult: you ' + result + '!');
-}
-
-function resetGame() {
-    userScore = 0;
-    compScore = 0;
-    document.getElementById('score').style.color = "black";
-    updateScoreText();
-    alert('scores reset!');
-}
-
-function updateScoreText() {
-    document.getElementById('score').innerText = "Score: " + userName + " " + userScore + " - " + compScore + " Computer";
 }
